b9897d3252a026dd63cc5c1a1b96e2cb24ab3ebc
Alvin Huang <59500784+dynamicdiscord@users.noreply.github.com>
2020-01-04 01:20:04 -0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,4 +7,12 @@ client = discord.Client()
 async def on_ready():
     print('Bot is ready and is logged on as {0.user}'.format(client))
 
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+
+    if message.content.startswith('!hello'):
+        await message.channel.send('Hello!')
+	   
 client.run('bot_token')
